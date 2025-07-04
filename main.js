c1733e9f87bb854fc92c407dab1f1015160d109d
BrunoMendes007 <bruno.neduziak.mendes@escola.pr.gov.br>
2025-07-04 12:21:46 +0000

    classlist

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i = 0; i< botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j = 0; i< botoes. length; j++){
             botoes[j].classlist.remove("ativo");
+             botoes[j].classlist.remove("ativo");
         }
         botoes[i].classlist.add("ativo");
+        textos[i].classlist.add("ativo");
     }
 }
