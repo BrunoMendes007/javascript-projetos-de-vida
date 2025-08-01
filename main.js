85e21cc775370c1867969e36d4a7350c72e4473b
BrunoMendes007 <bruno.neduziak.mendes@escola.pr.gov.br>
2025-08-01 10:45:56 +0000

    setInterval

--- main.js
+++ main.js
@@ -13,6 +13,11 @@ for(let i = 0; i< botoes.length; i++){
 }
 const contadores = document.querySelectorAll(".contador");
 const TempoObjetivol = new Date("2025-10-05T00:00:00");
+const TempoObjetivo2 = new Date("2025-12-05T00:00:00");
+const TempoObjetivo3 = new Date("2025-12-20T00:00:00");
+const TempoObjetivo4 = new Date("2026-02-05T00:00:00");
+
+const tempos = [TempoObjetivol, TempoObjetivo2, TempoObjetivo3, TempoObjetivo4]
 
 contadores [0] .textContent = TempoObjetivol - tempoAtual;
 
@@ -27,7 +32,23 @@ function calculaTempo(TempoObjetivo){
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
+    if (tempoFinal > 0 ){
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + "segundos"
+    }else{
+        return "Prazo Finalizado";
+    }
     
 }
+
+function atualizaCronometro(){
+    for (let = i = 0; i < contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+comecaCronometro();
